03de45544cbc3f6470e60294274d257e7b0d9815
Kai Wißler <kai.wissler@schwarzfisch.virtual-identity.net>
2019-05-21 17:13:16 +0200

    feat: Move GraphQL functionality to web component script

--- src/components/XWiki/XWiki.ts
+++ src/components/XWiki/XWiki.ts
@@ -1,59 +1,122 @@
 import Component from '@biotope/element';
 import template from './template';
 
+import ApolloClient from 'apollo-boost';
+import gql from 'graphql-tag';
+
 interface XWikiProps {
     items
 }
 
 interface XWikiState {
     items: any
+    liftId: string
 }
 
-const query = `
-    query {
-        allLifts {
-        id
-        }
-    }
-`;
-
-const url = "https://snowtooth.moonhighway.com/";
-const opts = {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({ query })
-};
-
-
- function returnJson() {
-    return fetch(url, opts)
-    .then(res => res.json());
+enum LiftStatus {
+    OPEN,
+    CLOSED,
+    HOLD
 }
 
-class XWiki extends Component< XWikiProps, XWikiState > {
+const client = new ApolloClient({
+    uri: 'https://snowtooth.moonhighway.com/'
+});
+
+class XWiki extends Component<XWikiProps, XWikiState> {
     static componentName = 'x-wiki';
 
+    private onEnterGet = (event) => {
+        if(event.keyCode === 13) {
+            event.preventDefault();
+            this.state.liftId = event.target.value;
+            this.getStatusForLiftId(this.state.liftId).then((data: any) => {
+                this.setState({ items: [data.data.Lift] })
+            })
+        }
+    }
+
+    private onEnterSet = (event) => {
+        const liftId = (<HTMLInputElement>this.shadowRoot.getElementById('liftIdForStatusSet')).value;
+        
+        let statusToSet;
+        switch(event.target.innerHTML) {
+            case 'open': statusToSet = 'OPEN'; break;
+            case 'close': statusToSet = 'CLOSED'; break;
+            case 'set on hold': statusToSet = 'HOLD'; break;
+        }
+        event.preventDefault();
+        this.setStatusForLiftId(liftId, statusToSet).then(() => {
+        }).then(() => {
+            client.cache.reset();
+            this.getStatusForAllLifts().then((data: any) => {
+                this.setState({ items: data.data.allLifts })
+            })
+        })
+    }
+
+    private getStatusForAllLifts = () => {
+        client.cache.reset();
+        return client.query({
+            query: gql`
+                query {
+                    allLifts {
+                        name
+                        status
+                        capacity
+                        id
+                    }
+                }`
+        }).catch(error => {});
+    }
+
+    private getStatusForLiftId = (liftId: string) => {
+        client.cache.reset();
+        return client.query({
+            query: gql`
+                query {
+                    Lift(id: "${liftId}") {
+                        name
+                        status
+                        capacity
+                    }
+                }`
+        }).catch(error => {return {data: { Lift: {name: liftId, status: 'not a lift that we know of.'}}}});
+    }
+    private setStatusForLiftId = (liftId: string, status: string) => {
+        return client.mutate({
+            mutation: gql`
+                mutation {
+                    setLiftStatus(id: "${liftId}", status: ${status}) {
+                        name
+                        status
+                        capacity
+                    }
+                }`
+        }).catch(error => {});
+    }
+
     connectedCallback() {
-        returnJson().then(data => {
-            this.setState({items: data.data.allLifts})
+        this.getStatusForAllLifts().then((data: any) => {
+            this.setState({ items: data.data.allLifts })
         })
     }
     get defaultState() {
-        return{
-            items: []
+        return {
+            items: [],
+            liftId: 'jazz-cat'
         }
     }
     render() {
-
         const {
             items,
-          } = this.props;
-        
-    const renderProps: XWikiProps = {
-        items:  this.state.items
-    }
+        } = this.props;
 
-    return template(this.html, renderProps);
+        const renderProps: XWikiProps = {
+            items: this.state.items
+        }
+
+        return template(this.html, renderProps, this.onEnterGet, this.onEnterSet);
     }
 }
 
--- src/components/XWiki/template.ts
+++ src/components/XWiki/template.ts
@@ -4,14 +4,22 @@ interface XWikiTemplateData {
     items: any
 }
 
-export default (render: Function, data: XWikiTemplateData) => {
+export default (render: Function, data: XWikiTemplateData, onEnterGet: Function, onEnterSet: Function) => {
     return render`
         <style>${styles.toString()}</style>
+        <h2>Lift Status</h2>
         <div>
-        <div>
-          ${data.items.map(item => {
-               return `<h1>${item.id}</h1> `
+            ${data.items.map(item => {
+                return `<div class="statusContainer"><div class="status ${item.status.toLowerCase()}"></div><h3>${item.name} has a capacity of ${item.capacity}.</h3></div>`
             })}
         </div>
+        <select id="liftIdForStatusSet">
+            ${data.items.map(item => {
+                return `<option value="${item.id}">${item.name}</option>`
+            })}
+        </select>
+        <a onclick=${onEnterSet}>open</a>
+        <a onclick=${onEnterSet}>close</a>
+        <a onclick=${onEnterSet}>set on hold</a>
     `;
 }
